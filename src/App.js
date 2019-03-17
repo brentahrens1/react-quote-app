35ad66a9a0a8e601b3f8c8e343876dd206be7028
Brent Ahrens <cidida@gmail.com>
2019-03-17 15:15:17 -0700

    react-quote-app

--- src/App.js
+++ src/App.js
@@ -3,23 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+   quotes: [
+    {
+    author: 'Pickle Juice Johnson',
+    quote: 'Dont pee in the pool fam'
+    },
+    {
+    author: 'Pickle Juice Peggy',
+    quote: 'Whenever you need me Ill be there'
+    },
+    {
+    author: 'Pickle Juice Betty',
+    quote: 'I feel like Rick James'
+    },
+    {
+    author: 'Pickle Juice Todd',
+    quote: 'Life is not in chronalogical order meaning you need to move around a bit and explore'
+    }],
+    index:0
+  }
+
+  randomQuote = ()=>{
+   var number = Math.floor(Math.random() * (this.state.quotes.length))
+   this.setState({
+     index: number
+   })
+    
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>{ this.state.quotes[this.state.index].quote }</h1>
+        <button type="submit" onClick={this.randomQuote}>Generate Random Quote</button>
       </div>
     );
   }
